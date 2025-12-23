49ea29706d67fd67efc49f1d6597653339990b6a
Sami-AlThobiti <72894279+Sami-AlThobiti@users.noreply.github.com>
2025-12-23 12:39:32 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        cairo: ['Cairo', 'sans-serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in-out',
+        'bounce-in': 'bounceIn 0.8s cubic-bezier(0.8, 0, 1, 1)',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        bounceIn: {
+          '0%, 100%': { transform: 'translateY(-5%)', animationTimingFunction: 'cubic-bezier(0.8,0,1,1)' },
+          '50%': { transform: 'translateY(0)', animationTimingFunction: 'cubic-bezier(0,0,0.2,1)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
